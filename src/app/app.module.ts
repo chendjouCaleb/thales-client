a1864f3b183459068fe1baae7cc9d4a6b88d7433
chendjou2016 <chendjou2016@outlook.fr>
2023-08-29 18:46:24 +0100

    Http error interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from "@angular/material/form-field";
 import {MatNativeDateModule} from "@angular/material/core";
 import {ApplicationHttpModule} from "./http";
 import {ApplicationServiceModule} from "./services";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {AuthenticationService, IdentityModule} from "./identity";
+import {IdentityModule} from "./identity";
 import {CustomerPickerModule} from "@app/Components";
-import {MatIconRegistry} from "@angular/material/icon";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import { registerLocaleData } from "@angular/common";
+import {registerLocaleData} from "@angular/common";
 import localeFr from '@angular/common/locales/fr'
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {ErrorCatchingInterceptor} from "@app/http-error.interceptor";
 
 
 registerLocaleData(localeFr, 'fr');
@@ -36,7 +37,12 @@ registerLocaleData(localeFr, 'fr');
   ],
   providers: [
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
-    {provide: LOCALE_ID, useValue: 'fr' }
+    {provide: LOCALE_ID, useValue: 'fr' },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorCatchingInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/http-error.interceptor.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+@Injectable()
+export class ErrorCatchingInterceptor implements HttpInterceptor {
+
+  constructor(private snackbar: MatSnackBar) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMsg = '';
+          if (error.error instanceof ErrorEvent) {
+            console.log('This is client side error');
+            errorMsg = `Error: ${error.error.message}`;
+          } else {
+            console.log('This is server side error');
+            errorMsg = `Error Code: ${error.status},  Message: ${error.error.message}`;
+          }
+          console.log(errorMsg);
+          this.snackbar.open(errorMsg, '', {duration: 5000})
+          return throwError(errorMsg);
+        })
+      )
+  }
+}
